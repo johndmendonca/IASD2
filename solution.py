c9b4bc2057e1ff0acdd750ecf71f9f3589c792d4
JohnM <mfsmm.98@gmail.com>
2019-11-15 15:51:53 +0000

    project setup v2

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 import probability
+import sys
 
 
 class Problem:
@@ -7,16 +8,68 @@ class Problem:
         """ Place here your code to load problem from opened file object fh
             and use probability.BayesNet() to create the Bayesian network"""
 
+        building = Building()
+
+        for ln in (ln for ln in fh.readlines() if len(ln.split()) > 0):
+            l_array = ln.split()
+            # Set of Rooms
+            if l_array[0] == 'R':
+                return NotImplementedError
+
+            # Set of connections
+            elif l_array[0] == 'C':
+                return NotImplementedError
+
+            # Set of sensors
+            elif l_array[0] == 'S':
+                return NotImplementedError
+
+            # Propagation Probabilities
+            elif l_array[0] == 'P':
+                return NotImplementedError
+
+            # Measurement
+            elif l_array[0] == 'M':
+                return NotImplementedError
+
+            else:
+                raise RuntimeError("Bad Format Error")
+
+
+        net = probability.BayesNet(node_specs)
+
     def solve(self):
         """ Place here your code to determine the maximum likelihood solution
             returning the solution room name and likelihood
             use probability.elimination_ask() to perform probabilistic inference"""
+        prob = 0
+        likelihood = 0
+
+        for rooms in museum:
+            res = probability.elimination_ask(rooms).show_approx()
+            if prob <= res.prob:
+                room = rooms
+                likelihood = res.prob
 
         return (room, likelihood)
 
 
 def solver(input_file):
     return Problem(input_file).solve()
+
+
+class Building:
+
+    def __init__(self):
+        self.rooms = []
+        self.connections = []
+        self.sensors = []
+
+    def add_room(self,room):
+        self.room.add(room)
+
+    def add_connection(self,connection):
+        self.connections.add(connection)
 
 
 """main"""
@@ -27,7 +80,7 @@ def main():
         with open(sys.argv[1], 'r') as f:
             sol = solver(f)
             f.close()
-
+            print("Solution:", sol)
     else:
         print("Usage:", sys.argv[0], "<filename>")
 
