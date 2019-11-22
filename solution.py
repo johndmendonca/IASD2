27fba54d7358f118ef01645e2bf870271becec09
JohnM <mfsmm.98@gmail.com>
2019-11-22 21:15:59 +0000

    Initial implementation.

--- solution.py
+++ solution.py
@@ -7,69 +7,120 @@ class Problem:
     def __init__(self, fh):
         """ Place here your code to load problem from opened file object fh
             and use probability.BayesNet() to create the Bayesian network"""
-
-        building = Building()
+        self.building = {}
+        self.alarms = {}
+        self.p = 1  # Reasonable default
+        self.measurements = []
+        self.net = probability.BayesNet()
+        self.meas_cnt = 0
 
         for ln in (ln for ln in fh.readlines() if len(ln.split()) > 0):
             l_array = ln.split()
             # Set of Rooms
             if l_array[0] == 'R':
-                return NotImplementedError
+                for room in l_array[1:]:
+                    self.building[room] = Room()
 
             # Set of connections
             elif l_array[0] == 'C':
-                return NotImplementedError
+                for word in l_array[1:]:
+                    connection = word.split(',')
+                    self.building[connection[0]].connections.add(connection[1])
+                    self.building[connection[1]].connections.add(connection[0])
 
             # Set of sensors
             elif l_array[0] == 'S':
-                return NotImplementedError
+                for info in l_array[1:]:
+                    sensor = info.split(':')
+                    self.alarms[sensor[0]] = sensor[1:]
 
             # Propagation Probabilities
             elif l_array[0] == 'P':
-                return NotImplementedError
+                self.p = float(l_array[1])
 
             # Measurement
             elif l_array[0] == 'M':
-                return NotImplementedError
+                self.meas_cnt += 1
+                meas_t = []
+                for meas in l_array[1:]:
+                    aux = meas.split(':')
+                    aux.append(self.meas_cnt)
+                    meas_t.append(aux)
+                self.measurements.append(meas_t)
 
             else:
                 raise RuntimeError("Bad Format Error")
 
+        var = []
+        t = 0
+        # for each time instant
+        for meas_t in self.measurements:
+            t += 1
+            measured_rooms = {}
+            # Formatting for BayesNet()
+            for meas in meas_t:
+                sensor_room = self.alarms[meas[0]][0]
+                meas.insert(1, '')
+                if meas[2] == 'F':
+                    meas[2] = 0
+                elif meas[2] == 'T':
+                    meas[2] = 1
+                else:
+                    raise RuntimeError("Bad Format Error")
+                meas[0] = meas[0] + '_' + str(t)
 
-        net = probability.BayesNet(node_specs)
+                measured_rooms[sensor_room] = meas[0]
+                self.net.add(meas)
+                var.append(meas)
+
+            parents = []
+            # Check each room for incoming prob at this, and previous time instants
+            for room_name, room in self.building.items():
+                # Check if the room has been estimated at t-1
+                if room_name + '_' + str(t-1) in var:
+                    parents.append(room_name + '_' + str(t-1))
+                # Check if adjacent rooms have been estimated at t-1
+                for adj_room in room.connections:
+                    if adj_room + '_' + str(t-1) in var:
+                        parents.append(adj_room + '_' + str(t-1))
+                # Check if sensor exists at t
+                if room_name in measured_rooms.keys():
+                    parents.append(measured_rooms[room_name])
+
+                # TODO: Add room inference to net. implies calculating intersection probabilities below:
+                """ P(room_t | room_(t-1)) = 1
+                    P(room_t | adj_room_(t-1)) = P
+                    P(room_t | sx_t) = TPR
+                    P(room_t | ¬sx_t) = FPR """
+
+                self.net.add(room_name + '_' + str(t), parents, {})
+                var.append(room_name + '_' + str(t))
 
     def solve(self):
         """ Place here your code to determine the maximum likelihood solution
             returning the solution room name and likelihood
             use probability.elimination_ask() to perform probabilistic inference"""
-        prob = 0
         likelihood = 0
+        highest_p_room = None
 
-        for rooms in museum:
-            res = probability.elimination_ask(rooms).show_approx()
-            if prob <= res.prob:
-                room = rooms
+        for room in self.building.keys():
+            # TODO: seems this only calculates a conditional probability depending on parents nodes
+            res = probability.elimination_ask(room + ' ' + str(self.meas_cnt), ' ', self.net).show_approx()
+            if likelihood <= res.prob:
+                highest_p_room = room
                 likelihood = res.prob
 
-        return (room, likelihood)
+        return highest_p_room, likelihood
 
 
 def solver(input_file):
     return Problem(input_file).solve()
 
 
-class Building:
+class Room:
 
     def __init__(self):
-        self.rooms = []
-        self.connections = []
-        self.sensors = []
-
-    def add_room(self,room):
-        self.room.add(room)
-
-    def add_connection(self,connection):
-        self.connections.add(connection)
+        self.connections = set()
 
 
 """main"""
